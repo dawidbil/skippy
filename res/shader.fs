d3838ba5296d2e8a222f7e43561c7fa139fe9db5
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-01-16 22:23:23 +0100

    add second texture

--- res/shader.fs
+++ res/shader.fs
@@ -4,9 +4,11 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
-uniform sampler2D ourTexture;
+uniform sampler2D texture0;
+uniform sampler2D texture1;
 
 void main()
 {
-	FragColor = texture(ourTexture, TexCoord);
+	vec2 negateTexCoord = vec2(-TexCoord.x, TexCoord.y);
+	FragColor = mix(texture(texture0, TexCoord), texture(texture1, negateTexCoord), 0.2);
 }
